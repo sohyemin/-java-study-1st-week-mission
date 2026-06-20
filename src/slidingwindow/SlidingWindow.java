01e255c30a7e1848dff70cc02fcb4717ba45f887
HYEM <sohyem.dev@gmail.com>
2026-06-21 02:16:18 +0900

    SlidingWindow 대충 구현해봄

--- src/slidingwindow/SlidingWindow.java
+++ src/slidingwindow/SlidingWindow.java
@@ -1,4 +1,35 @@
 package slidingwindow;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SlidingWindow {
+    public static void main(String[] args){
+        /*특정 범위 내의 합이 가장 큰 조합 찾기*/
+        int[] arr1 = {1, 15, 1, 2, 6, 12, 5, 7};
+        int windowsize = 3;
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        int currentSum =0;
+
+        for(int i=0; i<windowsize; i++){
+            queue.add(arr1[i]);
+            currentSum+=arr1[i];
+        }
+
+        int maxsum = currentSum;
+
+        for (int i = windowsize; i < arr1.length; i++) {
+            currentSum -= queue.remove();
+
+            queue.add(arr1[i]);
+            currentSum += arr1[i];
+
+            if(currentSum > maxsum)
+                maxsum=currentSum;
+        }
+
+        System.out.println(queue);
+    }
 }
